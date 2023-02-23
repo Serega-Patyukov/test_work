457ba1046f49f8bee6446d63dc7c20a8f53d0758
serega-patyukov <serega-patyukov@mail.ru>
2023-02-23 09:11:51 +0300

    init

---
+++ src/main/java/ru/patyukov/ligatestwork/LigaTestWorkApplication.java
@@ -0,0 +1,13 @@
+package ru.patyukov.ligatestwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigaTestWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigaTestWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/patyukov/ligatestwork/LigaTestWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.patyukov.ligatestwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigaTestWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
